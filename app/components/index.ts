ad8a162e35d2ba3f6878cdaa4cddaed96d83a718
Hrayr Movsisyan <iamhrayr@gmail.com>
2020-02-15 17:26:35 +0400

    Basic components fixed, added basic new component <Text />, <Input />

---
+++ app/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from './Button';
+export { default as Text } from './Text';
+export { default as Input } from './Input';
+export { default as Spacer } from './Spacer';
+export { default as Container } from './Container';
--- app/theme/dark.ts
+++ app/theme/dark.ts
@@ -2,9 +2,6 @@ import colors from './colors';
 // import base from './base';
 
 export default {
-  container: {
-    padding: 15,
-  },
   colors: {
     primary: colors.blue,
     secondary: colors.orange,
@@ -16,19 +13,35 @@ export default {
     link: colors.blue,
     white: colors.white,
   },
-  fontSizes: { sm: 14, md: 17, lg: 20, h3: 20, h2: 24, h1: 28, giant: 50 },
+  // TODO: may be base styles should be separated?
+  container: {
+    padding: 15,
+    bgColor: colors.white,
+  },
+  text: {
+    color: colors.darkGray,
+    fontSizes: { sm: 14, md: 17, lg: 20, h3: 20, h2: 24, h1: 28, giant: 50 },
+  },
   borderRadius: {
     square: 0,
     round: 7,
     circle: 50,
   },
+  spacer: { xs: 5, sm: 10, md: 15, lg: 20, xl: 25 },
   button: {
     fontWeight: 500,
     fontSizes: { sm: 14, md: 17, lg: 20 },
     paddingX: { sm: 18, md: 26, lg: 34 },
     paddingY: { sm: 8, md: 12, lg: 16 },
-    spacer: { xs: 5, sm: 10, md: 15, lg: 20, xl: 25 },
   },
-  // TODO: base styles should be separated
-  // TODO: base styles should be separated
+  form: {
+    label: {
+      color: colors.darkGray,
+      opacity: 0.5,
+    },
+    border: {
+      color: colors.gray,
+      width: 1,
+    },
+  },
 };
